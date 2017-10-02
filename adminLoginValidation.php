41688675c1c98b5b9719015387ea02d1e0b562b0
Bankai2212 <31263655+Bankai2212@users.noreply.github.com>
2017-10-02 15:26:16 +0800

    update admin login

---
+++ adminLoginValidation.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "EzCarCareDB";
+	$con = new mysqli($servername, $username, $password, $dbname);
+	
+	//Admin Validation
+	$AdminName = $_POST['AdminName'];
+	$AdminPassword = $_POST['AdminPassword'];
+	
+	$sql = "SELECT * from Admin WHERE userName = '$AdminName' AND password = '$AdminPassword'";
+	$result = mysqli_query($con, $sql);
+	
+	if (mysqli_num_rows($result) > 0)
+	{
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['AdminName'] = $row['userName'];
+		header('location:adminWelcome.php');
+	}
+	else
+	{
+		header('location:invalidLogin.html');
+	}
+	
+	mysqli_close($con);
+?>
