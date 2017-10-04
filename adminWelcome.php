e6056b56169fcb0f324cbf8bcaf13d957efca555
hole99 <31263658+hole99@users.noreply.github.com>
2017-10-04 14:48:23 +0800

    calendar

--- adminWelcome.php
+++ adminWelcome.php
@@ -151,7 +151,88 @@
 			</fieldset>
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="height:20px"></div>
 		</div>		
-		
+		<div class="container">
+				<div class="panel-group" id="calendarBox">
+					<div class="panel panel-default">
+						<div class="panel-heading" id="calendarPanel">
+							<h4 class="panel-title">
+								<a data-toggle="collapse" data-parent="#calendarBox" href="#openBox"> Calendar </a>
+							</h4>
+						</div>
+						<div id="openBox" class="panel-collapse collapse">
+							<div class="panel-body">
+								<script type="text/javascript">
+			var day_of_week = new Array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
+			var month_of_year = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+			var Calendar = new Date();
+			var year = Calendar.getFullYear();
+			var month = Calendar.getMonth();
+			var today = Calendar.getDate();
+			var weekday = Calendar.getDay();
+			var DAYS_OF_WEEK = 7; 
+			var DAYS_OF_MONTH = 31;
+			var cal;
+
+			Calendar.setDate(1);
+			Calendar.setMonth(month);
+			var TR_start = "<tr>";
+			var TR_end = "</tr>";
+			var highlight_start = '<td width="30"><table cellspacing="0" border="1"  style="border-color:#CCCCCC;background-color:#DEDEFF"><tr><td width="20" style="text-align:center;font-weight:bold">';
+			var highlight_end   = '</td></tr></table>';
+			var TD_start = '<td width="30" style="text-align:center">';
+			var TD_end = "</td>";
+			
+			cal =  '<table width="100%" height="200px" border="1" cellspacing="0" cellpadding="0" style="border-color:#BBBBBB"><tr><td>';
+			cal += '<table width="100%" height="200px" border="1" cellspacing="0" cellpadding="2">' + TR_start;
+			cal += '<td colspan="' + DAYS_OF_WEEK + '" style="background-color:#EFEFEF;text-align:center;font-weight:bold">';
+			cal += month_of_year[month]  + "  " + year + '</B>' + TD_end + TR_end;
+			cal += TR_start;
+			
+			for(var index=0; index < DAYS_OF_WEEK; index++)
+			{
+				if(weekday == index)
+					cal += TD_start + '<b>' + day_of_week[index] + '</b>' + TD_end;
+				else
+					cal += TD_start + day_of_week[index] + TD_end;
+			}
+
+			cal += TD_end + TR_end;
+			cal += TR_start;
+			
+			for(var index=0; index < Calendar.getDay(); index++)
+				cal += TD_start + "" + TD_end;
+
+			for(var index=0; index < DAYS_OF_MONTH; index++)
+			{
+				if( Calendar.getDate() > index )
+				{
+					week_day =Calendar.getDay();
+					if(week_day == 0)
+						cal += TR_start;
+
+					if(week_day != DAYS_OF_WEEK)
+					{
+						var day  = Calendar.getDate();
+							if( today==Calendar.getDate() )
+								cal += highlight_start + day + highlight_end + TD_end;
+							else
+								cal += TD_start + day + TD_end;
+					}
+					if(week_day == DAYS_OF_WEEK)
+						cal += TR_end;
+				}
+
+				Calendar.setDate(Calendar.getDate()+1);	
+			}
+
+			cal += '</td></tr></table></table>';
+			document.write(cal);
+			</script>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		<footer>
 			<div id="footerbar">
 				<div class="container">
