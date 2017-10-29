092abcab11d79c215e89f8e98e62a1f4e784d3f8
Bankai2212 <31263655+Bankai2212@users.noreply.github.com>
2017-10-29 17:52:07 +0800

    customer login complete

---
+++ custLoginValidation.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "EzCarCareDB";
+	$con = new mysqli($servername, $username, $password, $dbname);
+	
+	//Admin Validation
+	$CustomerName = $_POST['CustomerName'];
+	$CustomerPassword = $_POST['CustomerPassword'];
+	
+	$sql = "SELECT * from Customer WHERE userName = '$CustomerName' AND password = '$CustomerPassword'";
+	$result = mysqli_query($con, $sql);
+	
+	if (mysqli_num_rows($result) > 0)
+	{
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['CustomerName'] = $row['userName'];
+		header('location:userWelcome.php');
+	}
+	else
+	{
+		header('location:invalidLogin.html');
+	}
+	
+	mysqli_close($con);
+?>
