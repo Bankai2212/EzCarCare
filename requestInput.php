8a57d6939e81fad10a239428420a77fce4a17f2f
Bankai2212 <31263655+Bankai2212@users.noreply.github.com>
2017-10-29 20:48:57 +0800

    request php processing complete

--- requestInput.php
+++ requestInput.php
@@ -9,19 +9,35 @@
 	if(isset($_POST['submit']))
 	{
 		//Saving request details into database
-		$techName = $_POST['techName'];
-		$techTelNo = $_POST['techTelNo'];
-		$techAddress = $_POST['techAddress'];
-		$techState = $_POST['techState'];
-		$techEmail = $_POST['techEmail'];
-		$specialty = $_POST['specialty'];
+		$customerID = $_SESSION["CustomerID"];
+		$serviceID = $_POST['service'];
+		$description = $_POST['description'];
+		$date = $_POST['date'];
+		$time = $_POST['time'];
+		$carModel = $_POST['carModel'];
+		$requestAddress;
+		if($_POST['address'] == "default")
+		{
+			$sql2 = "SELECT address from Customer where customerID='$customerID'";
+			$result2 = mysqli_query($con, $sql2);
+											
+			if (mysqli_num_rows($result) > 0)
+			{
+				$row = mysqli_fetch_assoc($result);
+				$requestAddress = $row["address"];
+			}						
+		}
+		else
+		{
+			$requestAddress = $_POST["newAddress"];
+		}
 		
-		$sql = "INSERT INTO Technician (techName, techTelNo, techAddress, techState, techEmail, specialty) 
-			VALUES ('$techName', '$techTelNo', '$techAddress', '$techState', '$techEmail', '$specialty')";
+		$sql = "INSERT INTO Request (description, date, time, carModel, requestAddress, status, serviceID, customerID) 
+			VALUES ('$description', '$date', '$time', '$carModel', '$requestAddress', 'Pending', '$serviceID', '$customerID')";
 		mysqli_query($con, $sql);
 	}
 	
 	mysqli_close($con);
-	echo '<script type="text/javascript">alert("The new technician has been recorded successfully!");';
-	echo 'location.href="recordTechnician.html";</script>';
+	echo '<script type="text/javascript">alert("The new request has been recorded successfully!");';
+	echo 'location.href="requestService.php";</script>';
 ?>
